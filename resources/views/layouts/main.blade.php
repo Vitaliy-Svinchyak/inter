276881c4e7c9140f863fc6b5a8bc31e2c8503776
Vitaliy-Svinchyak <opirus6229078@mail.ru>
2016-11-29 21:22:02 +0200

    Using npm for dependencies

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,7 +8,7 @@
     <title>Inter</title>
 
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
-    <link rel="stylesheet prefetch" href="{{ asset('/cdn/angular-material.css') }}">
+    <link rel="stylesheet prefetch" href="{{ asset('/node_modules/angular-material/angular-material.min.css') }}">
     {{--<link rel="stylesheet prefetch"--}}
     {{--href="https://cdn.gitcdn.link/cdn/angular/bower-material/v1.1.1/angular-material.css">--}}
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
@@ -22,43 +22,35 @@
             <div ui-view></div>
         </div>
     </div>
+</div>
 
-    <script src="{{ asset('/cdn/angular.js') }}"></script>
-    <script src="{{ asset('/cdn/angular-animate.min.js') }}"></script>
-    <script src="{{ asset('/cdn/angular-cookies.min.js') }}"></script>
-    <script src="{{ asset('/cdn/angular-aria.min.js') }}"></script>
-    <script src="{{ asset('/cdn/angular-messages.min.js') }}"></script>
-    <script src="{{ asset('/cdn/angular-material.js') }}"></script>
-    <script src="{{ asset('/cdn/angular-ui-router.js') }}"></script>
-    <script src="{{ asset('/cdn/angular.textangular@1.5.0(textAngular-rangy.min.js+textAngular-sanitize.min.js+textAngular.min.js).js') }}"></script>
-
-    {{--<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.js"></script>--}}
-    {{--<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-animate.min.js"></script>--}}
-    {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.5/angular-cookies.min.js"></script>--}}
-    {{--<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-aria.min.js"></script>--}}
-    {{--<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-messages.min.js"></script>--}}
-    {{--<script src="https://cdn.gitcdn.link/cdn/angular/bower-material/v1.1.1/angular-material.js"></script>--}}
-    {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.3.2/angular-ui-router.js"></script>--}}
-    {{--<script src='http://cdn.jsdelivr.net/g/angular.textangular@1.5.0(textAngular-rangy.min.js+textAngular-sanitize.min.js+textAngular.min.js)'></script>--}}
+<script src="{{ asset('/node_modules/angular/angular.min.js') }}"></script>
+<script src="{{ asset('/node_modules/angular-animate/angular-animate.min.js') }}"></script>
+<script src="{{ asset('/node_modules/angular-cookies/angular-cookies.min.js') }}"></script>
+<script src="{{ asset('/node_modules/angular-aria/angular-aria.min.js') }}"></script>
+<script src="{{ asset('/node_modules/angular-messages/angular-messages.min.js') }}"></script>
+<script src="{{ asset('/node_modules/angular-material/angular-material.min.js') }}"></script>
+<script src="{{ asset('/node_modules/angular-ui-router/release/angular-ui-router.min.js') }}"></script>
+<script src='http://cdn.jsdelivr.net/g/angular.textangular@1.5.0(textAngular-rangy.min.js+textAngular-sanitize.min.js+textAngular.min.js)'></script>
 
 
-    <script src="{{ asset('/js/app.js') }}"></script>
-    <script src="{{ asset('/config/routes.js') }}"></script>
-    <script src="{{ asset('/bootstrap/main.js') }}"></script>
-    <script src="{{ asset('/controllers/IndexController.js') }}"></script>
-    <script src="{{ asset('/controllers/AuthController.js') }}"></script>
-    <script src="{{ asset('/controllers/RegistrationController.js') }}"></script>
-    <script src="{{ asset('/controllers/MyPageController.js') }}"></script>
-    <script src="{{ asset('/controllers/CreateQuestionController.js') }}"></script>
-    <script src="{{ asset('/controllers/MyMenuController.js') }}"></script>
-    <script src="{{ asset('/controllers/QuestionModalController.js') }}"></script>
-    <script src="{{ asset('/components/comment/controllers/CommentComponentController.js') }}"></script>
-    <script src="{{ asset('/components/question/controllers/QuestionComponentController.js') }}"></script>
-    <script src="{{ asset('/components/main-menu/controllers/MainMenuComponentController.js') }}"></script>
-    <script src="{{ asset('/models/User.js') }}"></script>
-    <script src="{{ asset('/services/QuestionService.js') }}"></script>
-    <script src="{{ asset('/services/AnswerRatingService.js') }}"></script>
-    <script src="{{ asset('/services/AnswerService.js') }}"></script>
-    <script src="{{ asset('/services/Logger.js') }}"></script>
+<script src="{{ asset('/js/app.js') }}"></script>
+<script src="{{ asset('/config/routes.js') }}"></script>
+<script src="{{ asset('/bootstrap/main.js') }}"></script>
+<script src="{{ asset('/controllers/IndexController.js') }}"></script>
+<script src="{{ asset('/controllers/AuthController.js') }}"></script>
+<script src="{{ asset('/controllers/RegistrationController.js') }}"></script>
+<script src="{{ asset('/controllers/MyPageController.js') }}"></script>
+<script src="{{ asset('/controllers/CreateQuestionController.js') }}"></script>
+<script src="{{ asset('/controllers/MyMenuController.js') }}"></script>
+<script src="{{ asset('/controllers/QuestionModalController.js') }}"></script>
+<script src="{{ asset('/components/comment/controllers/CommentComponentController.js') }}"></script>
+<script src="{{ asset('/components/question/controllers/QuestionComponentController.js') }}"></script>
+<script src="{{ asset('/components/main-menu/controllers/MainMenuComponentController.js') }}"></script>
+<script src="{{ asset('/models/User.js') }}"></script>
+<script src="{{ asset('/services/QuestionService.js') }}"></script>
+<script src="{{ asset('/services/AnswerRatingService.js') }}"></script>
+<script src="{{ asset('/services/AnswerService.js') }}"></script>
+<script src="{{ asset('/services/Logger.js') }}"></script>
 </body>
 </html>
